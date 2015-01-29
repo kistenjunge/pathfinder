ee77576ca36fad7fef0df00fce99ee71bb834cd7
kistenjunge <trash@boy-in-a-box.de>
2015-01-29 11:28:47 +0100

    worked on database and tests * mysql model file added * dev db against in memory * test against mysql * added restassured, guava and jsr305 * ignoring bak files

--- src/main/java/kistenjunge/org/entity/Project.java
+++ src/main/java/kistenjunge/org/entity/Project.java
@@ -1,20 +1,36 @@
 package kistenjunge.org.entity;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-public class Project
+public class Project implements Serializable
 {
-
-  @Column(name = "title", nullable = false, length = 100)
-  String name;
-
-  @Column(name = "description", nullable = true, length = 500)
-  String description;
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
+
+  @Column(name = "title", nullable = false, length = 100)
+  private String name;
+
+  @Column(name = "description", nullable = true, length = 500)
+  private String description;
+
+  /**
+   * protected no-args ctor
+   */
+  protected Project()
+  {
+  }
+
+  public Project(@Nonnull String name, @Nullable String description)
+  {
+    this.name = name;
+    this.description = description;
+  }
 
   public String getName()
   {
--- src/test/java/kistenjunge/org/controller/ProjectRestControllerTest.java
+++ src/test/java/kistenjunge/org/controller/ProjectRestControllerTest.java
@@ -11,17 +11,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
 import javax.inject.Inject;
-
 import java.util.Arrays;
 
-import static com.jayway.restassured.RestAssured.when;
-import static org.junit.Assert.*;
-import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
@@ -34,17 +29,15 @@ public class ProjectRestControllerTest
 
   @Value("${local.server.port}")
   int port;
-
-  Project alpha;
-  Project beta;
-
+  final Project alpha = new Project("alpha", "description for alpha");
+  final Project beta = new Project("beta", null);
   final String endpoint = "/api/project";
 
   @Before
   public void setUp() throws Exception
   {
-    initFixtures();
-    initRepository();
+    repository.deleteAll();
+    repository.save(Arrays.asList(alpha, beta));
 
     RestAssured.port = port;
   }
@@ -57,21 +50,6 @@ public class ProjectRestControllerTest
     .then()
         .statusCode(org.apache.http.HttpStatus.SC_OK)
         .body("name", Matchers.hasItems("alpha", "beta"));
-
   }
 
-  private void initFixtures()
-  {
-    alpha = new Project();
-    alpha.setName("alpha");
-    alpha.setDescription("description for alpha");
-    beta = new Project();
-    beta.setName("beta");
-  }
-
-  private void initRepository()
-  {
-    repository.deleteAll();
-    repository.save(Arrays.asList(alpha, beta));
-  }
 }
